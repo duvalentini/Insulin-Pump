90c603537a2fe964d4545908c361b8563242b515
al28o <aloverfield@yahoo.com>
2016-05-25 19:48:55 -0400

    Added two fragments: 1) ScenarioSelection 2)AvatarSelection
    Also added the xmls for the fragments. Changed main activity and main activity xmls. No errors, but havent built yet on tablet. Everything "should" work though.

---
+++ app/src/main/java/com/example/gestalt/insulinpumpulator/AvatarSelectionFragment.java
@@ -0,0 +1,20 @@
+package com.example.gestalt.insulinpumpulator;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aloverfield on 5/25/16.
+ */
+
+public class AvatarSelectionFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_avatar_selection, container, false);
+    }
+}
--- app/src/main/java/com/example/gestalt/insulinpumpulator/MainPageActivity.java
+++ app/src/main/java/com/example/gestalt/insulinpumpulator/MainPageActivity.java
@@ -1,17 +1,56 @@
 package com.example.gestalt.insulinpumpulator;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private Fragment scenarioSelection;
+    private Fragment avatarSelection;
+    private Button scenarioSelectionButton;
+    private Button avatarSelectionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        scenarioSelection = new Fragment();
+        avatarSelection = new Fragment();
+
+        scenarioSelectionButton = (Button) findViewById(R.id.scenario_select);
+        avatarSelectionButton = (Button) findViewById(R.id.avatarSelectionFragment);
+
+        scenarioSelectionButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                fragmentTransaction.add(R.id.scenarioSelectionFragment, scenarioSelection);
+                fragmentTransaction.commit();
+            }
+        });
+
+        avatarSelectionButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                fragmentTransaction.add(R.id.avatarSelectionFragment, avatarSelection);
+                fragmentTransaction.commit();
+            }
+        });
     }
+
     Intent intent = getIntent();
 
     public void logout(View view){
