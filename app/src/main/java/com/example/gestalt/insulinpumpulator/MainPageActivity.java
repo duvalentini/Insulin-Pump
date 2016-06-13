8415933e43cef8f71a1d93140dde51ca69a94bd8
Max <yamixenobb@gmail.com>
2016-06-13 18:14:07 -0400

    Basic layout configuration

--- app/src/main/java/com/example/gestalt/insulinpumpulator/MainPageActivity.java
+++ app/src/main/java/com/example/gestalt/insulinpumpulator/MainPageActivity.java
@@ -1,27 +1,15 @@
 package com.example.gestalt.insulinpumpulator;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
-public class MainPageActivity extends FragmentActivity {
+public class MainPageActivity extends FragmentActivity implements MainPageNavButtons.OnFragmentInteractionListener {
 
     private String msg = "Android : ";
-    private int[] _buttonIDs = {R.id.bAccountInfo, R.id.bConnections, R.id.bCustomize, R.id.scenario_select};
-
-    private void selectView(int viewID) {
-        for (int i : _buttonIDs) {
-            Button b = (Button) findViewById(i);
-            if (i == viewID) {
-                b.setEnabled(false);
-            } else if (!b.isEnabled()) {
-                b.setEnabled(true);
-            }
-        }
-    }
 
     //for dynamoDB, will move later
 //    private IdentityManager identityManager;
@@ -47,7 +35,7 @@ public class MainPageActivity extends FragmentActivity {
             }
 
             // Create a new Fragment to be placed in the activity layout
-            AvatarSelectionFragment firstFragment = new AvatarSelectionFragment();
+            MainPageNavButtons firstFragment = new MainPageNavButtons();
 
             // In case this activity was started with special instructions from an
             // Intent, pass the Intent's extras to the fragment as arguments
@@ -56,8 +44,9 @@ public class MainPageActivity extends FragmentActivity {
             // Add the fragment to the 'fragment_container' FrameLayout
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_section, firstFragment).commit();
         }
+    }
 
-//        //for dynamoDB, will put somewhere else later
+    //        //for dynamoDB, will put somewhere else later
 //        credentialsProvider = identityManager.getCredentialsProvider();
 //        AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
 //        mapper = new DynamoDBMapper(ddbClient);
@@ -65,83 +54,27 @@ public class MainPageActivity extends FragmentActivity {
 //        book = new Book();
 //        book.setIsbn("1234567890");
 
-        //leave this commented out
+    //leave this commented out
 //        book.setHardCover(false);
 //        book.setTitle("Great Expectations");
 //        book.setAuthor("Charles Dickens");
 //        book.setPrice(1299);
 
+    public void swapFragment(Fragment fragment) { //add int containerID to specify which fragment container
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-        Button scenarioSelect = (Button) findViewById(R.id.scenario_select);
-        scenarioSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectView(R.id.scenario_select);
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
 
-                //Switch Fragment out
-                // Create new fragment and give it an argument specifying the article it should show
-                ScenarioSelectionFragment newFragment = new ScenarioSelectionFragment();
+        //dynamoDB test, will remove later
+//        if (fragment.getId() == R.id.bConnections) {
+//            //      mapper.save(book);
+//        }
+        transaction.replace(R.id.fragment_section, fragment);
+        transaction.addToBackStack(null);
 
-                //No arguments to set as of yet
-
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                // Replace whatever is in the fragment_container view with this fragment,
-                // and add the transaction to the back stack so the user can navigate back
-                transaction.replace(R.id.fragment_section, newFragment);
-//                transaction.addToBackStack(null);
-
-                // Commit the transaction
-                transaction.commit();
-                System.out.println("Scenario select pressed");
-            }
-        });
-        Button connections = (Button) findViewById(R.id.bConnections);
-        connections.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectView(R.id.bConnections);
-
-                //dynamoDB test, will remove later
-          //      mapper.save(book);
-
-                //Switch fragment out
-                System.out.println("Connections pressed");
-            }
-        });
-        Button customize = (Button) findViewById(R.id.bCustomize);
-        customize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectView(R.id.bCustomize);
-                //Switch Fragment out
-                // Create new fragment and give it an argument specifying the article it should show
-                AvatarSelectionFragment newFragment = new AvatarSelectionFragment();
-
-                //No arguments to set as of yet
-
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-                // Replace whatever is in the fragment_container view with this fragment,
-                // and add the transaction to the back stack so the user can navigate back
-                transaction.replace(R.id.fragment_section, newFragment);
-//                transaction.addToBackStack(null);
-
-                // Commit the transaction
-                transaction.commit();
-                System.out.println("Customize pressed");
-            }
-        });
-        Button accountInfo = (Button) findViewById(R.id.bAccountInfo);
-        accountInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectView(R.id.bAccountInfo);
-                //Switch fragment out
-                System.out.println("Account info pressed");
-            }
-        });
-        selectView(R.id.bAccountInfo);
+        // Commit the transaction
+        transaction.commit();
     }
 
     /** Called when the activity is about to become visible. */
@@ -177,5 +110,10 @@ public class MainPageActivity extends FragmentActivity {
     public void onDestroy() {
         super.onDestroy();
         Log.d(msg, "The onDestroy() event");
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/gestalt/insulinpumpulator/MainPageNavButtons.java
@@ -0,0 +1,153 @@
+package com.example.gestalt.insulinpumpulator;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainPageNavButtons.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainPageNavButtons#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainPageNavButtons extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MainPageNavButtons() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MainPageNavButtons.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MainPageNavButtons newInstance(String param1, String param2) {
+        MainPageNavButtons fragment = new MainPageNavButtons();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main_page_nav_buttons, container, false);
+
+        Button scenarioSelect = (Button) view.findViewById(R.id.scenario_select);
+        scenarioSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create fragment
+                ScenarioSelectionFragment newFragment = new ScenarioSelectionFragment();
+
+                //No arguments to set as of yet
+
+                //Swap fragments out
+                ((MainPageActivity) getActivity()).swapFragment(newFragment);
+                System.out.println("Scenario select pressed");
+            }
+        });
+        Button connections = (Button) view.findViewById(R.id.bConnections);
+        connections.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Switch fragment out
+                System.out.println("Connections pressed");
+            }
+        });
+        Button customize = (Button) view.findViewById(R.id.bCustomize);
+        customize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create new fragment
+                AvatarSelectionFragment newFragment = new AvatarSelectionFragment();
+
+                ((MainPageActivity) getActivity()).swapFragment(newFragment);
+                System.out.println("Customize pressed");
+            }
+        });
+        Button accountInfo = (Button) view.findViewById(R.id.bAccountInfo);
+        accountInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Switch fragment out
+                System.out.println("Account info pressed");
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
