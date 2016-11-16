d638b6412b0693318118681bc017c737f0678ef5
akhil1227 <akhil27@hotmail.com>
2016-11-16 15:41:06 -0500

    changes for new scenario with image files. also renamed first 2 scenarios so name doesn't give away solution

--- app/src/main/java/com/example/gestalt/insulinpumpulator/ScenarioPlaythroughFragment.java
+++ app/src/main/java/com/example/gestalt/insulinpumpulator/ScenarioPlaythroughFragment.java
@@ -268,7 +268,8 @@ public class ScenarioPlaythroughFragment extends Fragment implements AdapterView
                 ScenarioPlaythrough._playerScore -= 0;
             } else if (selectedOption._str.equals("Take a drink of water")) {
                 ScenarioPlaythrough._playerScore += 10;
-            } else {
+            }
+            else {
 
             }
         }
@@ -297,7 +298,7 @@ public class ScenarioPlaythroughFragment extends Fragment implements AdapterView
         // TODO: Change end values to what Eileen said
         // Go to scenes based on BG
 
-        if (_fileName.equals("Hypoglycemia")) {
+        if (_fileName.equals("Scenario1")) {
             if (ScenarioPlaythrough.mPump.bloodGlucose < 70) {
                 _currentSceneIndex = 5;
             }
@@ -325,7 +326,7 @@ public class ScenarioPlaythroughFragment extends Fragment implements AdapterView
             if (_currentSceneIndex == 2 || _currentSceneIndex == 1 || _currentSceneIndex == 0 || _currentSceneIndex == 5 || _currentSceneIndex == 6) {
                 ScenarioPlaythrough.mPump.passTime(15);
             }
-        } else if (_title.equals("Hyperglycemia")) {
+        } else if (_title.equals("Scenario2")) {
             if (ScenarioPlaythrough.mPump.bloodGlucose < 70) {
                 _currentSceneIndex = 5;
             }
@@ -356,8 +357,23 @@ public class ScenarioPlaythroughFragment extends Fragment implements AdapterView
             }
             // End game when BG is in target range
             if (ScenarioPlaythrough.mPump.bloodGlucose <250 ) {
+                _currentSceneIndex = 10;
+            }
+            if (selectedOption._str.equals("Check ketones") && ScenarioPlaythrough.mPump.bloodGlucose>250) {
+                _currentSceneIndex = 9;
+            }
+            if (selectedOption._str.equals("Proceed to ED") || selectedOption._str.equals("Start DKA protocol")) {
+                _currentSceneIndex = 12;
+                ScenarioPlaythrough._playerScore = 0;
+            }
+            if (selectedOption._str.equals("Do nothing and wait 1 hour") && ScenarioPlaythrough.mPump.bloodGlucose<250) {
+                _currentSceneIndex = 12;
+
+            }
+            if (selectedOption._str.equals("Bolus again and wait 1 hour") && ScenarioPlaythrough.mPump.bloodGlucose>250) {
                 _currentSceneIndex = 8;
             }
+
             if (ScenarioPlaythrough.mPump.bloodGlucose > 450) {
                 _currentSceneIndex = 8;
                 ScenarioPlaythrough._playerScore = 0;
